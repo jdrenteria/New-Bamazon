0d235d5d070985a7a822da88ab1ad2bebc702562
jdrenteria <jesusrenteria48@gmail.com>
2020-02-09 23:25:00 -0600

    added fucntion and code to prompt user to choose what product and quatity they would like

--- bamazon.js
+++ bamazon.js
@@ -18,9 +18,37 @@ connection.connect(function(err){
 var makeTable = function(){
     connection.query("SELECT * FROM products", function(err,res){
         for(var i=0; i<res.length; i++){
-            console.log(res[i].itemid+" || "+res[i].productname+" || "+
-            res[i].departmentname+" || "+res[i].price+" || "+res[i].stockquantity+"\n");
+            console.log(res[i].itemid+" || "+res[i].productname+" || "+res[i].departmentname+" || "+res[i].price+" || "+res[i].stockquantity+"\n");
+        }
+        promptCustomer(res);
+    })
+}
+var promptCustomer = function(res){
+    inquirer.prompt([{
+        type:'input',
+        name:'choice',
+        message: "What would you like to purchase? [Quit with Q]"
+    }]).then(function(answer){
+        var correct = false;
+        for(var i=0; i<res.length;i++){
+            if(res[i].productname==answer.choice){
+                correct=true;
+                var product=answer.choice;
+                var id=i;
+                inquirer.prompt({
+                    type:"input",
+                    name:"quant",
+                    message:"How many would you like to buy?",
+                    validate:function(value){
+                        if(isNaN(value)===false){
+                            return true;
+                        }else {
+                            return false;
+                        }
+                    }
+                }).then
+
+            }
         }
     })
 }
-
